e72db0fa61a3e0d80115dba671dd281bb510b9f5
Redoactive <ignasteris456@gmail.com>
2024-10-14 21:42:57 +0300

    added random color on mousehover

--- javascript.js
+++ javascript.js
@@ -18,7 +18,7 @@ function createGrid(maxSize){
         for (let x = 0; x < maxSize; x++) {
             const tile = document.createElement("div");
             tile.classList.add("tiles", "row");
-            tile.addEventListener("mouseover", () => tile.classList.add("darker"));
+            tile.addEventListener("mouseover", () => tile.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16));
             column.appendChild(tile);
         }
         
